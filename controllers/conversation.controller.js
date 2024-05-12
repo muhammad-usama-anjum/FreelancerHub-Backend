97d679b03494835f823c861a289e66fd18f0d69d
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-05-12 16:35:46 +0500

    Added backend for the website

---
+++ controllers/conversation.controller.js
@@ -0,0 +1,73 @@
+// user.controller.js
+import createError from "../utils/createError.js";
+import Conversation from "../models/conversation.model.js";
+import User from "../models/user.model.js"
+
+export const createConversation = async (req, res, next) => {
+  const newConversation = new Conversation({
+    id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+    sellerId: req.isSeller ? req.userId : req.body.to,
+    buyerId: req.isSeller ? req.body.to : req.userId,
+    readBySeller: req.isSeller,
+    readByBuyer: false,
+
+  })
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(201).send(savedConversation);
+  } catch (err) {
+    next(err)
+  }
+
+};
+
+export const updateConversation = async (req, res, next) => {
+  try {
+    const updatedConversation = await Conversation.findOneAndUpdate(
+      { id: req.params.id },
+      {
+        $set:
+        {
+          ...(req.isSeller ? {readBySeller:true}:{readByBuyer:true}),
+        }
+      },
+      { new: true }
+    );
+
+    res.status(200).send(updatedConversation);
+  } catch (err) {
+    next(err)
+  }
+
+};
+
+export const getSingleConversation = async (req, res, next) => {
+  try {
+    const conversation = await Conversation.findOne({ id: req.params.id })
+    if(!conversation)
+    {
+      return next(createError(404, "Not found!"));
+    }
+    res.status(200).send(conversation);
+  } catch (err) {
+    next(err)
+  }
+
+};
+
+export const getConversations = async (req, res, next) => {
+  try {
+    let conversations = await Conversation.find(
+      req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
+      ).sort({updatedAt:-1});
+    // conversations = conversations.forEach(async(element) => {
+    //   element["buyerName"] = (await User.findOne({_id:element.buyerId}))["username"]
+    //   console.log(element
+    res.status(200).send(conversations);
+  } catch (err) {
+    next(err)
+  }
+
+};
+
+
